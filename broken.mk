e3dee0a0bba0edebc047f6ec92ab471f8f5f16e6
gnoons <thenoons.g@gmail.com>
2016-05-14 10:39:48 -0400

    Update and rename cm.mk to broken.mk

--- broken.mk
+++ broken.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := broken_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
